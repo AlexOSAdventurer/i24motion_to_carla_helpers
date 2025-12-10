30f591483ca8ede1d61dbd64b6d4c8a60a8fbc14
Alex Richardson <william.a.richardson@vanderbilt.edu>
2025-12-10 03:23:09 -0600

    Fixed download script.

--- download_Unreal.sh
+++ download_Unreal.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git clone --depth 1 -b carla "https://$1@github.com/CarlaUnreal/UnrealEngine.git ./UnrealEngine_4.26/
+git clone --depth 1 -b carla "https://$1@github.com/CarlaUnreal/UnrealEngine.git" ./UnrealEngine_4.26/
